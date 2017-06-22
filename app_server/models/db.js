e625d362d52b670fb150fc9b4e1eb19eb7387e6b
Manuel Paniagua <paniagm8032@student.laccd.edu>
2017-06-22 22:00:11 +0000

    Configure database

---
+++ app_server/models/db.js
@@ -0,0 +1,55 @@
+var mongoose = require("mongoose");
+
+var dbURI = 'mongodb://' + process.env.IP;
+    if(process.env.NODE_ENV === 'production') {
+        dbURI = 'mongodb://demo:standup@ds161960.mlab.com:61960/standup';
+        // dbURI = process.env.MONGOLAB_URI;
+    }
+    
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error:' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+
+///////////////////////////////////////////////////
+// close mongoose connection on application end  //
+///////////////////////////////////////////////////
+var gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+// nodemon restart event
+process.once('SIGUSR2', function () {
+    gracefulShutdown('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+// app termination
+process.on('SIGINT', function () {
+    gracefulShutdown('app termination', function () {
+        process.exit(0);
+    });
+});
+
+// heroku termination
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app shutdown', function () {
+        process.exit(0);
+    });
+});
+
+require("./locations");

bc3eb5d28931f26cb0ac129cefc24879534bbc6b
Manuel Paniagua <paniagm8032@student.laccd.edu>
2018-01-25 06:47:45 +0000

    push new changes

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -12,7 +12,7 @@
         };
         
         vm.sidebar = {
-            content: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint?  Let Loc8r help you find the place you're looking for."
+            content: "App currently has a default location set.  I manually added restaurants and in order for them to show, I had to enter a nearby zip code."
         };
          vm.message = "Checking your location";
         
